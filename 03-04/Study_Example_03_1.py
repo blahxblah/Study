eba436a448a4be42e202bb1accc38ee1b916d745
blahxblah <id.blahxblah@gmail.com>
2023-06-03 22:19:35 +0900

    .

--- 03-04/Study_Example_03_1.py
+++ 03-04/Study_Example_03_1.py
@@ -1,11 +1,12 @@
 import random as rand
 
-money = [500, 100, 50, 10, 5, 1]
-cash = rand.randrange(1, 1001)
-c_cash = cash
+moneytype = [500, 100, 50, 10, 5, 1]
+price = rand.randrange(1, 1000)
+changes = 1000 - price
+cal_changes = changes
 count = 0
-for i in range(len(money)):
-    count = count + c_cash // money[i]
-    c_cash = c_cash % money[i]
+for i in range(len(moneytype)):
+    count = count + cal_changes // moneytype[i]
+    cal_changes = cal_changes % moneytype[i]
 
-print(f"cash : {cash} / count : {count}")
+print(f"price : {price} / changes : {changes} / count : {count}")
