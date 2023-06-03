d739602ac0241c8dd64aa1a6feb3604d3142900b
blahxblah <id.blahxblah@gmail.com>
2023-06-03 21:02:59 +0900

    test

---
+++ 03-04/Study_Example_03_1.py
@@ -0,0 +1,11 @@
+import random as rand
+
+money = [500, 100, 50, 10, 5, 1]
+cash = rand.randrange(1, 1001)
+c_cash = cash
+count = 0
+for i in range(len(money)):
+    count = count + c_cash // money[i]
+    c_cash = c_cash % money[i]
+
+print(f"cash : {cash} / count : {count}")
